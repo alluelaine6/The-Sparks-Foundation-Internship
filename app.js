d8d0c7ef9f849d316cc488f3cf34b68d2605c400
alluelaine6 <103167888+alluelaine6@users.noreply.github.com>
2022-06-20 12:50:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+});const menu = document.querySelector('#mobile-menu');
+const menuLst = document.querySelector('.navbar__menu')
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLst.classList.toggle('active');
